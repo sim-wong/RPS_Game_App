17d41e3bf491d9c1b4bdde2f651db87c20cd8980
Simon <simon.wong@archonsystems.com>
2021-04-29 01:11:56 -0400

    Add project files.

---
+++ RPS_Game_App/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace RPS_Game_App
+{
+    partial class RPS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissor = new System.Windows.Forms.Button();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.picCPU = new System.Windows.Forms.PictureBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.namePlayer = new System.Windows.Forms.Label();
+            this.nameCPU = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(60, 70);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(75, 23);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(60, 166);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(75, 23);
+            this.btnPaper.TabIndex = 0;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissor
+            // 
+            this.btnScissor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissor.Location = new System.Drawing.Point(60, 270);
+            this.btnScissor.Name = "btnScissor";
+            this.btnScissor.Size = new System.Drawing.Size(75, 23);
+            this.btnScissor.TabIndex = 0;
+            this.btnScissor.Text = "Scissor";
+            this.btnScissor.UseVisualStyleBackColor = true;
+            this.btnScissor.Click += new System.EventHandler(this.btnScissor_Click);
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.Image = global::RPS_Game_App.Properties.Resources.Animal_World;
+            this.picPlayer.Location = new System.Drawing.Point(206, 70);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(166, 223);
+            this.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayer.TabIndex = 1;
+            this.picPlayer.TabStop = false;
+            // 
+            // picCPU
+            // 
+            this.picCPU.Image = global::RPS_Game_App.Properties.Resources.Animal_World;
+            this.picCPU.Location = new System.Drawing.Point(607, 70);
+            this.picCPU.Name = "picCPU";
+            this.picCPU.Size = new System.Drawing.Size(166, 223);
+            this.picCPU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCPU.TabIndex = 1;
+            this.picCPU.TabStop = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(452, 166);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // namePlayer
+            // 
+            this.namePlayer.AutoSize = true;
+            this.namePlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.namePlayer.Location = new System.Drawing.Point(272, 54);
+            this.namePlayer.Name = "namePlayer";
+            this.namePlayer.Size = new System.Drawing.Size(42, 13);
+            this.namePlayer.TabIndex = 2;
+            this.namePlayer.Text = "Player";
+            // 
+            // nameCPU
+            // 
+            this.nameCPU.AutoSize = true;
+            this.nameCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameCPU.Location = new System.Drawing.Point(676, 54);
+            this.nameCPU.Name = "nameCPU";
+            this.nameCPU.Size = new System.Drawing.Size(32, 13);
+            this.nameCPU.TabIndex = 2;
+            this.nameCPU.Text = "CPU";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(409, 18);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(150, 20);
+            this.labelScore.TabIndex = 2;
+            this.labelScore.Text = "Player: 0 - CPU: 0";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(698, 370);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRules.Location = new System.Drawing.Point(206, 370);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(75, 23);
+            this.btnRules.TabIndex = 0;
+            this.btnRules.Text = "Rules";
+            this.btnRules.UseVisualStyleBackColor = true;
+            this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
+            // 
+            // RPS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::RPS_Game_App.Properties.Resources.table_background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(853, 450);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.nameCPU);
+            this.Controls.Add(this.namePlayer);
+            this.Controls.Add(this.picCPU);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.btnScissor);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "RPS";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rock_Paper_Scissors by Simon Wong v1.0.0";
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissor;
+        private System.Windows.Forms.PictureBox picPlayer;
+        private System.Windows.Forms.PictureBox picCPU;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label namePlayer;
+        private System.Windows.Forms.Label nameCPU;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRules;
+    }
+}
+
---
+++ RPS_Game_App/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPS_Game_App
+{
+    public partial class RPS : Form
+    {
+
+        string[] CPUchoiceList = { "rock", "paper", "scissor", "paper", "rock", "scissor" };
+
+        int randomNumber = 0;
+
+        Random rnd = new Random();
+
+        string CPUChoice;
+
+        string playerChoice;
+
+        int playerScore;
+        int CPUScore;
+
+        public RPS()
+        {
+            InitializeComponent();
+
+            playerChoice = "none";
+
+            playerScore = 0;
+            CPUScore = 0;
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.rock;
+            playerChoice = "rock";
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.paper;
+            playerChoice = "paper";
+        }
+
+        private void btnScissor_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.scissor;
+            playerChoice = "scissor";
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            randomNumber = rnd.Next(0, CPUchoiceList.Length);
+
+            CPUChoice = CPUchoiceList[randomNumber];
+
+            switch (CPUChoice)
+            {
+                case "rock":
+                    picCPU.Image = Properties.Resources.rock;
+                    {
+                        if (playerChoice == "scissor")
+                        {
+                            CPUScore += 1;
+
+                            MessageBox.Show("CPU Wins, Rock crushes Scissor");
+                        }
+                        else if (playerChoice == "paper")
+                        {
+                            playerScore += 1;
+
+                            MessageBox.Show("Player Wins, Paper covers Rock");
+                        }
+                        else if (playerChoice == "rock")
+                        {
+                            MessageBox.Show("It's a draw!");
+                        }
+                        else if (playerChoice == "none")
+                        {
+                            MessageBox.Show("Please choose a move");
+                        }
+                        labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+                        picCPU.Image = Properties.Resources.Animal_World;
+                        if (playerScore > 4)
+                        {
+                            MessageBox.Show("Congrats, You Win the War!");
+                            playerScore = 0;
+                            CPUScore = 0;
+                            labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                            playerChoice = "none";
+
+                            picPlayer.Image = Properties.Resources.Animal_World;
+                            picCPU.Image = Properties.Resources.Animal_World;
+                        }
+                        else if (CPUScore > 4)
+                        {
+                            MessageBox.Show("You lose! Better luck next time.");
+                            playerScore = 0;
+                            CPUScore = 0;
+                            labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                            playerChoice = "none";
+
+                            picPlayer.Image = Properties.Resources.Animal_World;
+                            picCPU.Image = Properties.Resources.Animal_World;
+                        }
+                        break;
+                    }
+                case "paper":
+                    picCPU.Image = Properties.Resources.paper;
+                    if (playerChoice == "scissor")
+                    {
+                        playerScore += 1;
+
+                        MessageBox.Show("Player Wins, Scissors cut Paper");
+                    }
+                    else if (playerChoice == "rock")
+                    {
+                        CPUScore += 1;
+
+                        MessageBox.Show("CPU Wins, Paper covers Rock");
+                    }
+                    else if (playerChoice == "paper")
+                    {
+                        MessageBox.Show("It's a draw!");
+                    }
+                    else if (playerChoice == "none")
+                    {
+                        MessageBox.Show("Please choose a move");
+                    }
+                    labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+                    picCPU.Image = Properties.Resources.Animal_World;
+                    if (playerScore > 4)
+                    {
+                        MessageBox.Show("Congrats, You Win the War!");
+                        playerScore = 0;
+                        CPUScore = 0;
+                        labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                        playerChoice = "none";
+
+                        picPlayer.Image = Properties.Resources.Animal_World;
+                        picCPU.Image = Properties.Resources.Animal_World;
+                    }
+                    else if (CPUScore > 4)
+                    {
+                        MessageBox.Show("You lose! Better luck next time.");
+                        playerScore = 0;
+                        CPUScore = 0;
+                        labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                        playerChoice = "none";
+
+                        picPlayer.Image = Properties.Resources.Animal_World;
+                        picCPU.Image = Properties.Resources.Animal_World;
+                    }
+                    break;
+                case "scissor":
+                    picCPU.Image = Properties.Resources.scissor;
+                    if (playerChoice == "rock")
+                    {
+                        playerScore += 1;
+
+                        MessageBox.Show("Player Wins, Rock crushes Scissor");
+                    }
+                    else if (playerChoice == "paper")
+                    {
+                        CPUScore += 1;
+
+                        MessageBox.Show("CPU Wins, Scissor cuts Paper");
+                    }
+                    else if (playerChoice == "scissor")
+                    {
+                        MessageBox.Show("It's a draw!");
+                    }
+                    else if (playerChoice == "none")
+                    {
+                        MessageBox.Show("Please choose a move");
+                    }
+                    labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+                    picCPU.Image = Properties.Resources.Animal_World;
+                    if (playerScore > 4)
+                    {
+                        MessageBox.Show("Congrats, You Win the War!");
+                        playerScore = 0;
+                        CPUScore = 0;
+                        labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                        playerChoice = "none";
+
+                        picPlayer.Image = Properties.Resources.Animal_World;
+                        picCPU.Image = Properties.Resources.Animal_World;
+                    }
+                    else if (CPUScore > 4)
+                    {
+                        MessageBox.Show("You lose! Better luck next time.");
+                        playerScore = 0;
+                        CPUScore = 0;
+                        labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+                        playerChoice = "none";
+
+                        picPlayer.Image = Properties.Resources.Animal_World;
+                        picCPU.Image = Properties.Resources.Animal_World;
+                    }
+                    break;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            CPUScore = 0;
+            labelScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+            playerChoice = "none";
+
+            picPlayer.Image = Properties.Resources.Animal_World;
+            picCPU.Image = Properties.Resources.Animal_World;
+        }
+
+        private void btnRules_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rock crushes Scissor, Scissor cuts Paper, and Paper covers Rock.");
+            MessageBox.Show("First to 5 wins!");
+        }
+    }
+}
